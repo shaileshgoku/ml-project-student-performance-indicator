5b14e31ac48fc28a5477ef99abd05bc98d97325c
SHAILESH S <shouturmight@gmail.com>
2026-04-27 20:25:05 +0530

    changed the return statement in model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -72,7 +72,7 @@ class ModelTrainer:
             predicted=best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
-            return r2_square
+            return r2_square, best_model_name, best_model
 
 
         except Exception as e:
